df8147300202a81271943620465b7c3c25bca154
Debora Cantuarias <d.cantuarias.urrutia@gmail.com>
2023-04-19 19:05:47 -0400

    cambio css

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -1,5 +1,5 @@
-const URL_BASE = 'https://rickandmortyapi.com/api';
-const URL_CHARACTERS = URL_BASE + '/character';
+const URL_BASE = 'https://digimon-api.vercel.app/api/digimon';
+const URL_CHARACTERS = URL_BASE + '/name';
 let contenido;
 let carta;
 
@@ -9,12 +9,9 @@ function tabla(datos) {
     for (let temp of datos) {
         contenido.innerHTML += ` <tr>
         <th scope="row">${temp.name}</ th>
-        <td><img width="100px" height="100px" src="${temp.image}"</td>
-        <td>${temp.species}</td>
+        <td><img width="150px" height="150px" src="${temp.img}"</td>
+        <td>${temp.level}</td>
     </tr> `;
-        if (temp.id == 40) {
-            break
-        }
 
     }
 }
@@ -24,15 +21,15 @@ function tarjeta(data) {
 
     for (let temp of data) {
         carta.innerHTML += ` 
-          <div id="tarjSola" class="card mb-3 container ps-0" style="max-width: 540px;">
+          <div id="tarjSola" class="card mb-3 container ps-0 mt-3 bg-secondary bg-gradient rounded-1" style="max-width: 540px;">
     <div class="row g-0">
       <div class="col-md-4">
-        <img src="${temp.image}" class="img-fluid rounded-start" alt="...">
+        <img src="${temp.img}" class="img-fluid rounded-start" alt="...">
       </div>
       <div class="col-md-8">
         <div class="card-body">
           <h5 class="card-title">"NOMBRE: ${temp.name}"</h5>
-          <p class="card-text">ESPECIE: "${temp.species}"</p>
+          <p class="card-text">ESPECIE: "${temp.level}"</p>
          </div>
       </div>
     </div>
@@ -49,13 +46,13 @@ function mostrarImagenes() {
     document.getElementById('galeria').style.display = 'block';
 
     img.innerHTML = "";
-    for (let temp of dataImagenes.results) {
+    for (let temp of dataImagenes) {
         img.innerHTML += `
             <div id="card" class="card">
-            <img src="${temp.image}" class="card-img-top" alt="imagen ${temp.name}"></img>
+            <img src="${temp.img}" class="card-img-top" alt="imagen ${temp.name}"></img>
             <div class="card-body">
                 <h6 class="card-title">${temp.name}</h6>
-                <p class="card-text">${temp.species}</p>
+                <p class="card-text">${temp.level}</p>
             </div>
             </div>
 `
@@ -69,23 +66,23 @@ function capturaDato() {
     document.getElementById('galeria').style.display = 'none';
     document.getElementById('carta').style.display = 'block';
 
-    fetch(URL_CHARACTERS + '?name=' + nombrePersonaje)
+    fetch("https://digimon-api.vercel.app/api/digimon/name/" + nombrePersonaje)
         .then(response => response.json())
         .then(datos => {
             console.log(datos);
-            tarjeta(datos.results);
+            tarjeta(datos);
         });
 }
 
 $(document).ready(function () {
     contenido = document.getElementById('contenido');
-    carta= document.getElementById('carta');
+    carta = document.getElementById('carta');
 
-    fetch(URL_CHARACTERS)
+    fetch(URL_BASE)
         .then(response => response.json())
         .then(datos => {
             console.log(datos);
-            tabla(datos.results);
+            tabla(datos);
             dataImagenes = datos;
         });
 
